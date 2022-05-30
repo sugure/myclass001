30e169779b8ca694050130bcedfea0257da4f849
Your Name <you@example.com>
2022-05-30 18:52:49 +0800

    V1

--- src/test.java
+++ src/test.java
@@ -1,6 +1,6 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("你好世界！");
-        System.out.println("不好");
+        System.out.println("好");
     }
 }
